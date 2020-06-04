7d2cc019cf21718bbbf6710a65a3f68c62dc3199
Pepeb <4borowik@gmail.com>
2020-06-04 19:50:42 +0200

    jeszcze szczelanie

---
+++ UltraviolentHelicopterOfDoom_PROTO/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public GameObject endGamePanel;
+    public GameObject endGamePanel2;
+    //public Text gameScore;
+    //public Text endScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endGamePanel.gameObject.SetActive(false);
+        endGamePanel2.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Finish")
+        {
+            //collision
+            //endScore.text = gameScore.text;
+            endGamePanel.gameObject.SetActive(true);
+            //Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "Obstacle")
+        {
+            //collision
+            //endScore.text = gameScore.text;
+            endGamePanel2.gameObject.SetActive(true);
+            //Destroy(gameObject);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ UltraviolentHelicopterOfDoom_PROTO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float playerSpeed = 5;
+    public float playerJump = 5;
+    //private float hDir;
+    //private float vDir;
+
+    private bool jump = false;
+    private bool ground = false;
+
+    private void Start()
+    {      
+
+        rb = GetComponent<Rigidbody2D>();       
+
+        
+    }
+
+    private void Update()
+    {
+
+        transform.position += new Vector3(1, 0, 0) * Time.deltaTime * playerSpeed;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && ground)
+        {
+            jump = true;
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (jump)
+        {
+            rb.AddForce(new Vector2(0f, playerJump), ForceMode2D.Impulse);
+            jump = false;
+            ground = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            ground = true;
+        }
+    }
+
+}
